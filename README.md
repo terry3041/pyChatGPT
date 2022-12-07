0937683efda03f83a786a5aecfaa0f272dbbc074
terry3041 <oo20108@gmail.com>
2022-12-07 17:32:08 +0800

    Add support for auth with credentials

--- README.md
+++ README.md
@@ -28,9 +28,10 @@ python -m pyChatGPT
 from pyChatGPT import ChatGPT
 
 session_token = 'abc123'  # `__Secure-next-auth.session-token` cookie from https://chat.openai.com/chat
-api = ChatGPT(session_token)
-api2 = ChatGPT(session_token, conversation_id='conversation_id')  # specify a conversation id
-api3 = ChatGPT(session_token, proxy='http://proxy.example.com:8080')  # specify proxy
+api = ChatGPT(session_token)  # auth with session token
+api2 = ChatGPT(email='example@domain.com', password='password')  # auth with email and password
+api3 = ChatGPT(session_token, conversation_id='some-random-uuid')  # specify a conversation id
+api4 = ChatGPT(session_token, proxy='http://proxy.example.com:8080')  # specify proxy
 
 resp = api.send_message('Hello, world!')
 print(resp['message'])
--- src/pyChatGPT/pyChatGPT.py
+++ src/pyChatGPT/pyChatGPT.py
@@ -1,6 +1,7 @@
 import requests
 import uuid
 import json
+import re
 
 
 class ChatGPT:
@@ -9,24 +10,108 @@ class ChatGPT:
     '''
 
     def __init__(
-        self, session_token: str, conversation_id: str = None, proxy: str = None
+        self,
+        session_token: str = None,
+        email: str = None,
+        password: str = None,
+        conversation_id: str = None,
+        proxy: str = None,
     ) -> None:
         '''
         Initialize the ChatGPT class\n
+        Either provide a session token or email and password\n
         Parameters:
-        - session_token: Your session token in cookies named as `__Secure-next-auth.session-token` from https://chat.openai.com/chat
+        - session_token: (optional) Your session token in cookies named as `__Secure-next-auth.session-token` from https://chat.openai.com/chat
+        - email: (optional) Your OpenAI email
+        - password: (optional) Your OpenAI password
         - conversation_id: (optional) The conversation ID if you want to continue a conversation
-        - proxies: (optional) The proxy to use, in the format `http://ip:port` or `https://ip:port`
+        - proxy: (optional) The proxy to use, in URL format (i.e. `https://ip:port`)
         '''
-        self.session_token = session_token
         self.conversation_id = conversation_id
         self.parent_id = str(uuid.uuid4())
         self.proxies = {'http': proxy, 'https': proxy} if proxy else {}
         self.headers = {
-            'Cookie': f'__Secure-next-auth.session-token={self.session_token}',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.62',
         }
+        self.session_token = session_token
+        if not self.session_token:
+            if not email or not password:
+                raise ValueError('No session token or login credentials are provideddd')
+            self.session_token = self._login(email, password)
+        self.headers[
+            'Cookie'
+        ] = f'__Secure-next-auth.session-token={self.session_token}'
         self.refresh_auth()
+
+    def _login(self, email: str, password: str) -> str:
+        '''
+        Login to OpenAI\n
+        Parameters:
+        - email: Your OpenAI email
+        - password: Your OpenAI password\n
+        Returns the session token
+        '''
+        session = requests.Session()
+        session.headers = self.headers
+        session.proxies = self.proxies
+
+        # Get the CSRF token
+        resp = session.get('https://chat.openai.com/api/auth/csrf')
+        csrf_token = resp.json()['csrfToken']
+
+        # Get state
+        resp = session.post(
+            'https://chat.openai.com/api/auth/signin/auth0?prompt=login',
+            data={'callbackUrl': '/', 'csrfToken': csrf_token, 'json': 'true'},
+        )
+        if resp.status_code != 200:
+            raise ValueError(f'Status code {resp.status_code}: {resp.text}')
+        redirect_url = resp.json()['url']
+
+        # Redirect to auth0 /login/identifier
+        resp = session.get(redirect_url)
+        if resp.status_code != 200:
+            raise ValueError(f'Status code {resp.status_code}: {resp.text}')
+        pattern = r'<input type="hidden" name="state" value="(.*)" \/>'
+        results = re.findall(pattern, resp.text)
+        if not results:
+            raise ValueError(f'Could not get state: {resp.text}')
+        state = results[0]
+
+        # Post email
+        resp = session.post(
+            f'https://auth0.openai.com/u/login/identifier?state={state}',
+            data={
+                'state': state,
+                'username': email,
+                'js-available': 'false',
+                'webauthn-available': 'true',
+                'is-brave': 'false',
+                'webauthn-platform-available': 'false',
+                'action': 'default',
+            },
+        )
+        if resp.status_code != 200:
+            raise ValueError(f'Status code {resp.status_code}: {resp.text}')
+
+        # Post password
+        resp = session.post(
+            f'https://auth0.openai.com/u/login/password?state={state}',
+            data={
+                'state': state,
+                'username': email,
+                'password': password,
+                'action': 'default',
+            },
+        )
+        if resp.status_code != 200:
+            raise ValueError(f'Status code {resp.status_code}: {resp.text}')
+
+        # Get session token in CookieJar
+        cookies = session.cookies.get_dict()
+        if '__Secure-next-auth.session-token' not in cookies:
+            raise ValueError(f'Could not get session token: {cookies}')
+        return cookies['__Secure-next-auth.session-token']
 
     def refresh_auth(self) -> None:
         '''
